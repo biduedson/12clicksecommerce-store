548351ca50e5e2a14bd88a641bc96e0079c8d588
biduedson <112134265+biduedson@users.noreply.github.com>
2023-11-06 18:07:27 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div
-      className="
-  text-red-500"
-    >
-      hello world
-    </div>
-  );
+  return <div></div>;
 }
