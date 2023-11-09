2cca92dd586638337dcb8602ddee5d56602775af
biduedson <112134265+biduedson@users.noreply.github.com>
2023-11-09 14:37:02 -0300

    fix: ensure correct produt total price is calculated

--- src/helpers/product.ts
+++ src/helpers/product.ts
@@ -14,11 +14,11 @@ export const computeProductTotalPrice = (
     };
   }
 
-  const totalPrice =
+  const totadiscount =
     Number(product.basePrice) * (product.discountPercentage / 100);
 
   return {
     ...product,
-    totalPrice,
+    totalPrice: Number(product.basePrice) - totadiscount,
   };
 };
